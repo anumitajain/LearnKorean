a32a825f545bd931cb11f7abaa904367675459e4
Meghanhacker16 <38823522+Meghanhacker16@users.noreply.github.com>
2018-04-29 05:56:48 -0400

    hh

---
+++ graphics.py
@@ -0,0 +1,39 @@
+#Graphics
+#-----------------------------------------------
+from pygame import *
+#-----------------------------------------------
+rec = ((1200, 800))
+screen = display.set_mode(rec)
+screen.fill((255, 255, 255))
+#-----------------------------------------------
+blueArrowPic = image.load("blueArrow.png")
+screen.blit(blueArrowPic, (300, 150))
+micPic = image.load("micPic.jpg")
+screen.blit(micPic, (530, 300))
+#-----------------------------------------------
+micRect = Rect(530, 300, 140, 140)
+txtRect = Rect(400, 150, 400, 100)
+statusRect = Rect(500, 600, 200, 50)
+draw.rect(screen, (0), micRect, 2)
+draw.rect(screen, (0), txtRect, 2)
+draw.rect(screen, (0), statusRect, 2)
+#-----------------------------------------------
+running=True
+while running:
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+        if e.type == MOUSEBUTTONDOWN:
+            triPic = screen.copy()
+            if e.button == 1:
+               linePic = screen.copy()
+               lineStartx,lineStarty = e.pos
+
+    mb = mouse.get_pressed()
+    mx, my = mouse.get_pos()
+#-------------------------------------------------
+    
+
+#--------------------------------------------------
+    display.flip()
+quit()
--- korean.py
+++ korean.py
@@ -1,8 +1,52 @@
 # Korean speaking app
-import codecs
-f1 = codecs.open(file1, "r","utf-8")
-text = f1.read()
-print(type(text))
-print(text.encode('utf-8'))
+##import codecs
+##f1 = codecs.open(file1, "r","utf-8")
+##text = f1.read()
+##print(type(text))
+##print(text.encode('utf-8'))
+#----------------------------------------------------------
+# convert speech to text
+
+import SpeechRecognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+#-----------------------------------------------------------
+# speech to text example from
+# https://stackoverflow.com/questions/40549198/text-editor-which-can-convert-speech-to-text-and-vice-versa?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+from SpeechRecognition import * 
+from pyttsx import * 
+
+speech_engine = pyttsx.init('sapi5') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
+speech_engine.setProperty('rate', 150)
+
+def speak(text):
+    speech_engine.say(text)
+    speech_engine.runAndWait()
+
+recognizer = SpeechRecognition.Recognizer()
+
+def listen():
+    with SpeechRecognition.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        return recognizer.recognize_sphinx(audio)
+        # or: return recognizer.recognize_google(audio)
+    except SpeechRecognition.UnknownValueError:
+        print("Could not understand audio")
+    except SpeechRecognition.RequestError as e:
+        print("Recog Error; {0}".format(e))
+
+    return ""
+
+speak("Say something!")
+speak("I heard you say " + listen())
+
 
 
