13bb729ae895ca92b0be6919491253bd37e7e994
Meghanhacker16 <38823522+Meghanhacker16@users.noreply.github.com>
2018-04-29 06:33:52 -0400

    random

--- speechrecognition.py
+++ speechrecognition.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 import time
 # obtain audio from the microphone
 r = sr.Recognizer()
+
 while True:
     with sr.Microphone() as source:
         print("Say something!")
@@ -13,10 +14,7 @@ while True:
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
         splitData = r.recognize_google(audio, language = "ko").lower().split()
-        if 'hello' in splitData:
-            print("hi")
         print(splitData)
-        time.sleep(5)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
