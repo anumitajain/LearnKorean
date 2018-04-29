edaffc3eb54d57c970f5dc6942f677159de84fad
anumitajain <38822754+anumitajain@users.noreply.github.com>
2018-04-29 03:19:13 -0400

    Mr.McKenzie's  cyberbit

---
+++ unicode.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pygame import *
+init()
+one_moment = "等一下"
+easy = "그것은 쉽다"
+screen = display.set_mode((640,480))
+fnt = font.Font("cyberbit.ttf",60)
+txtPic = fnt.render(easy,True,(255,255,255))
+screen.blit(txtPic,(100,100))
+
+while running:
+    for evnt in event.get():                
+        if evnt.type == QUIT:
+            running = False
+
+    display.flip()
+
+quit()
