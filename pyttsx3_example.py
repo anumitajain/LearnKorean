32634737f3bb9676cd8e08621d8fe754c48b76e5
anumitajain <38822754+anumitajain@users.noreply.github.com>
2018-05-04 09:45:08 -0400

    trying to change language to Korean

--- pyttsx3_example.py
+++ pyttsx3_example.py
@@ -1,5 +1,8 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("jal jinaess-eoyo")
+languages = engine.getProperty('languages')
+for language in languages:
+    engine.setProperty('language', language.id)
+    engine.say("my name is bob")
 engine.runAndWait()
 
---
+++ pyttsxchange to korean attempt.py
@@ -0,0 +1,6 @@
+import pyttsx3
+
+pyttsx3.init()
+
+class pyttsx3.voice.Voice:
+    setLang = languages["ko-KR"]
