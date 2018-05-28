b0c6e9043c38b6a88dedf76c6de3bb8a5d45a0b2
Meghanhacker16 <38823522+Meghanhacker16@users.noreply.github.com>
2018-05-28 12:49:06 -0400

    Who knows...

--- basket game.py
+++ basket game.py
@@ -1,38 +1,94 @@
 #Basket game.
-#----------------------------------
+#Meghan Tran & Anumita Jain
 from pygame import *
-#----------------------------------
-screen = display.set_mode((1200, 800))
-#----------------------------------
-basket = image.load("basket.jpg")
-#----------------------------------
-bx = 0
+from random import *
 
+font.init()
+init()
+screen = display.set_mode((1200, 800))
+
+
+#IMAGES
+basket = image.load("basket.jpg")
+
+#FONT
+fnt = font.Font("Cyberbit.ttf", 50)
+fntBasket = font.SysFont("Comic Sans MS", 29, False, False)
+
+#MOVEBASKET
+bx = 0
 def moveBasket(bx):
-    keys = key.get_pressed()
-    
+    keys = key.get_pressed()    
     if keys[K_LEFT] and bx >= 10:
-        bx -= 10
+        bx -= 5
     if keys[K_RIGHT] and bx < 1040:
-        bx += 10
+        bx += 5
     return bx
+
+#BLITLETTER
+def blitLetter(wordsA):
+    txtPic = fnt.render(wordsA[x],True,(0,0,0))
+    return txtPic
     
+
+#DRAWINGLETTERS
+def drawingLetters(fallingLets,fallingY):    
+    for i in range(6):
+        txtPic1 = fnt.render(fallingLets[i],True,(0,0,0))
+        screen.blit(txtPic1, (i*200+100, fallingY[i]))
+
+#SWITCHLETTERS
+##def switchLetters():
+    
+    
+#RUNNING LOOP    
 running=True
+ty = 0
+listFalling_koreanCh = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㄲ", "ㄸ", "ㅃ", "ㅆ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
+listBasket_englishPh = ["g/k", "n/n", "d/t", "r/l", "m/m", "b/p", "s/t", "-/ng", "j/t", "kk/k", "tt/-", "pp/-", "ss/t", "jj/-", "ch/t", "k/k", "t/t", "p/p", "h/h", "a", "ae", "ya", "yae", "eo", "e", "yeo", "ye", "o", "wa", "wae", "eo", "yo", "u", "wo", "we", "wi", "yu", "eu", "ui", "i"] 
+#if you get listBasket_englishPh[0:18] wrong, we should put a hint saying: "Remember, in listBasket_englishPh[0], it sounds like a g when it's on top of the vowel, but like a k when on the bottom." 
+onBasket = choice(listBasket_englishPh)
+onBasketTxt = fntBasket.render(onBasket,True,(0,0,0))
+lenOnBasket = 80 - int(len(onBasket)//2)*15
+KoIndex = int(listBasket_englishPh.index(onBasket))
+fallingLets = []
+fallingLets.append(listFalling_koreanCh[KoIndex])
+
+myClock = time.Clock()
+
+while len(fallingLets)<6:        
+    let = choice(listFalling_koreanCh)
+    if let not in fallingLets:
+        fallingLets.append(let)
+
+shuffle(fallingLets)
+
+fallingY = [randint(-500,0) for i in range(6)]
+
 while running:
     for e in event.get():
         if e.type == QUIT:
-            running = False
-
+            running = False            
     mx, my = mouse.get_pos()
     mb = mouse.get_pressed()
     bx = moveBasket(bx)
     
     screen.fill((255,255,255))
     screen.blit(basket, (bx, 640))
+    onBasketTxt = fntBasket.render(onBasket,True,(0,0,0))
+    screen.blit(onBasketTxt, (bx+lenOnBasket, 640+78))
+    
+    drawingLetters(fallingLets,fallingY)
+    for i in range(6):
+        fallingY[i] += 1
 
-    myClock.tick(100)
+    if 711 - fallingY == 0:
+        
+                
+    print(fallingLets)
+    print(KoIndex)
+    
+    myClock.tick(60)    
     display.flip()
 quit()
-    
-    
     
--- graphics.py
+++ graphics.py
@@ -1,27 +1,50 @@
-#Graphics
+#LAnguage learning project
+#modes: start page, beginner, intermediate, advanced
+#help page/instructions, concentration, falling words
+#Anumita Jain and Meghan Tran
 #-----------------------------------------------
 from pygame import *
 from random import *
 import speech_recognition as sr
 import time
 #-----------------------------------------------
-screen = display.set_mode((1200,800))
-screen.fill((255, 255, 255))
-#------------------------------------------------
 font.init()
 init()
 Fontt = font.SysFont("Comic Sans MS", 20, False, False)
 smallfont = font.SysFont("Comic Sans MS", 15, False, False)
+screen = display.set_mode((1200,800))
+screen.fill((255, 255, 255))
+#------------------------------------------------
+#FONT LOADING
 correct = Fontt.render("Correct!", True, (0, 255, 0))
 incorrect = Fontt.render("Incorrect!", True, (255, 0, 0))
 Pronounce= Fontt.render("Try to pronounce this word.", True, (0,0,0))
 startRecording = Fontt.render("^CLICK to start recording.", True, (0,0,0))
 NoUnderstand = smallfont.render("Google Speech Recognition could not understand audio.", True, (0,0,0))
+#<<<<<<< HEAD
+#=======
 
+#IMAGE LOADING
+blueArrowPic = image.load("blueArrow.png")    
+micPic = image.load("micPic.jpg")
+
+#RECTS
+beginnerRect = Rect(80, 200, 500, 120)
+micRect = Rect(530, 300, 140, 140)
+KtxtRect = Rect(400, 150, 400, 100)
+correctRect = Rect(500, 600, 200, 50)
+statusRect = Rect(400, 480, 400, 80)
+    
+
+mode = "Start Page"  #different modes so different code chunks are used
+#-----------------------------------------------
+#>>>>>>> b37069600e70d21e3f7ffd73c2510bd6e86204ef
 blueArrowPic = image.load("blueArrow.png")
 heartPic = image.load("heart.png")    
 micPic = image.load("micPic.jpg")
+
 gameOver = image.load("game over.png")
+
     
 #------------------------------------------------------
 def record():
@@ -37,8 +60,6 @@ def record():
         string = r.recognize_google(audio, language = "ko").lower()
         #print(type(splitData))
         #string = " ".join(splitData)
-        #text = fnt.render(splitData[0], True, (0,0,0))
-        #verbal = screen.blit(text,(400, 465))
         return string
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
@@ -50,84 +71,142 @@ def record():
 fnt = font.Font("cyberbit.ttf",60)
 words = ["안녕","여보세요","안녕","내 이름은","잘 지냈어요"]
 definitions =["hi","hello","bye", "My name is", "How are you"]
-
-
-running=True
-    
+running= True
 while running:
-    screen.fill((255,255,255))
-    a = randint(0,4)
-    screen.blit(Pronounce, (400, 120))
-    screen.blit(startRecording, (465, 442))
-    #----------------------------------------------
-    
-    screen.blit(blueArrowPic, (300, 150))
-    screen.blit(micPic, (530, 300))
-    #-----------------------------------------------
-    micRect = Rect(530, 300, 140, 140)
-    txtRect = Rect(400, 150, 400, 100)
-    correctRect = Rect(500, 600, 200, 50)
-    statusRect = Rect(400, 480, 400, 80)
-    heart = [880, 0, 0, 73]
-    draw.rect(screen, (0), micRect, 2)
-    draw.rect(screen, (0), txtRect, 2)
-    draw.rect(screen, (255,255,255), correctRect, 2)
-    draw.rect(screen, (0), statusRect, 2)
-    txtPic = fnt.render(words[a],True,(0,0,0))
-    screen.blit(txtPic,(400,150))
-    incorrectRect = Rect(500, 600, 200, 50)
-    screen.blit(heartPic, (880, 0))
-    screen.blit(heartPic, (960, 0))
-    screen.blit(heartPic, (1040, 0))
-    screen.blit(heartPic, (1120, 0))
+    recording = False
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+        if e.type == MOUSEBUTTONDOWN:
+            click = True
+            if e.button == 1:
+                linePic = screen.copy()
+                lineStartx,lineStarty = e.pos
 
-    while running:
-        recording = False
-        for e in event.get():
-            if e.type == QUIT:
-                running = False
-            if e.type == MOUSEBUTTONDOWN:
-                click = True
-                if e.button == 1:
-                   linePic = screen.copy()
-                   lineStartx,lineStarty = e.pos
+    mb = mouse.get_pressed()
+    mx, my = mouse.get_pos()
+    if mode == "Start Page":
+        draw.rect(screen, (0, 0, 255), beginnerRect)
+        if beginnerRect.collidepoint(mx,my) and click:
+            mode = "Beginner"
 
-        mb = mouse.get_pressed()
-        mx, my = mouse.get_pos()
-        if micRect.collidepoint(mx,my):
-            if mb[0] == 1:
-                recording = True  
-                draw.rect(screen, (255,255,255), (465,442, 303, 30))
-                recordnotif = Fontt.render("Recording...", True, (0,0,0))
-                screen.blit(recordnotif,(465,442))
-                string = record()
-                spokentext = fnt.render(string, True, (0,0,0))
-                screen.blit(spokentext, (400,465))
-                if string == words[a]:
-                    screen.blit(correct, (500, 600))
-                    display.flip()
-                    time.sleep(2)
-                    draw.rect(screen, (255,255,255), incorrectRect)
-                    break
-                else:
-                    if heart[2] == 320:
-                        screen.blit(gameOver, (0, 0))
-                    else:
-                        screen.blit(incorrect, (500, 600))
-                        display.flip()
-                        time.sleep(2)
-                        draw.rect(screen, (255,255,255), statusRect)
-                        draw.rect(screen, (0,0,0), statusRect, 2)
-                        draw.rect(screen, (255,255,255), incorrectRect)
-                        heart[2]+=80
-                        draw.rect(screen, (255,255,255), heart)                                        
-            else:
+    if mode == "Beginner":
+
+        running=True
+        while running:
+            screen.fill((255,255,255))
+            a = randint(0,4)
+            screen.blit(Pronounce, (400, 120))
+            screen.blit(startRecording, (465, 442))
+            screen.blit(blueArrowPic, (300, 150))
+            screen.blit(micPic, (530, 300))
+
+        #-----------------------------------------------
+            draw.rect(screen, (0), micRect, 2)
+            draw.rect(screen, (0), KtxtRect, 2)
+           #draw.rect(screen, (0), correctRect, 2)
+            draw.rect(screen, (0), statusRect, 2)
+            txtPic = fnt.render(words[a],True,(0,0,0))
+            screen.blit(txtPic,(400,150))
+            while running:
+                    recording = False
+                    for e in event.get():
+                        if e.type == QUIT:
+                            running = False
+                        if e.type == MOUSEBUTTONDOWN:
+                            click = True
+                            if e.button == 1:
+                               linePic = screen.copy()
+                               lineStartx,lineStarty = e.pos
+
+                    mb = mouse.get_pressed()
+                    mx, my = mouse.get_pos()
+                    if micRect.collidepoint(mx,my):
+                        if mb[0] == 1:
+                            recording = True  
+                            draw.rect(screen, (255,255,255), (465,442, 303, 30))
+                            recordnotif = Fontt.render("Recording...", True, (0,0,0))
+                            screen.blit(recordnotif,(465,442))
+                            string = record()
+                            spokentext = fnt.render(string, True, (0,0,0))
+                            screen.blit(spokentext, (400,465))
+                            if string == words[a]:
+                                screen.blit(correct, (500, 600))
+                                break
+                            else:
+                                screen.blit(incorrect, (500, 600))
+                            
+                        else:
+                            recording = False
+                            draw.rect(screen, (255,255,255), (465,442, 303, 30))
+                            screen.blit(startRecording, (465, 442))
+                        
+
+            #-----------------------------------------------
+            micRect = Rect(530, 300, 140, 140)
+            txtRect = Rect(400, 150, 400, 100)
+            correctRect = Rect(500, 600, 200, 50)
+            statusRect = Rect(400, 480, 400, 80)
+            heart = [880, 0, 0, 73]
+            draw.rect(screen, (0), micRect, 2)
+            draw.rect(screen, (0), txtRect, 2)
+            draw.rect(screen, (255,255,255), correctRect, 2)
+            draw.rect(screen, (0), statusRect, 2)
+            txtPic = fnt.render(words[a],True,(0,0,0))
+            screen.blit(txtPic,(400,150))
+            incorrectRect = Rect(500, 600, 200, 50)
+            screen.blit(heartPic, (880, 0))
+            screen.blit(heartPic, (960, 0))
+            screen.blit(heartPic, (1040, 0))
+            screen.blit(heartPic, (1120, 0))
+            running = True
+            while running:
                 recording = False
-                draw.rect(screen, (255,255,255), (465,442, 303, 30))
-                screen.blit(startRecording, (465, 442))
-            
-            
-        
+                for e in event.get():
+                    if e.type == QUIT:
+                        running = False
+                    if e.type == MOUSEBUTTONDOWN:
+                        click = True
+                        if e.button == 1:
+                           linePic = screen.copy()
+                           lineStartx,lineStarty = e.pos
+
+                mb = mouse.get_pressed()
+                mx, my = mouse.get_pos()
+                if micRect.collidepoint(mx,my):
+                    if mb[0] == 1:
+                        recording = True  
+                        draw.rect(screen, (255,255,255), (465,442, 303, 30))
+                        recordnotif = Fontt.render("Recording...", True, (0,0,0))
+                        screen.blit(recordnotif,(465,442))
+                        string = record()
+                        spokentext = fnt.render(string, True, (0,0,0))
+                        screen.blit(spokentext, (400,465))
+                        if string == words[a]:
+                            screen.blit(correct, (500, 600))
+                            display.flip()
+                            time.sleep(2)
+                            draw.rect(screen, (255,255,255), incorrectRect)
+                            break
+                        else:
+                            if heart[2] == 320:
+                                screen.blit(gameOver, (0, 0))
+                            else:
+                                screen.blit(incorrect, (500, 600))
+                                display.flip()
+                                time.sleep(2)
+                                draw.rect(screen, (255,255,255), statusRect)
+                                draw.rect(screen, (0,0,0), statusRect, 2)
+                                draw.rect(screen, (255,255,255), incorrectRect)
+                                heart[2]+=80
+                                draw.rect(screen, (255,255,255), heart)                                        
+                    else:
+                        recording = False
+                        draw.rect(screen, (255,255,255), (465,442, 303, 30))
+                        screen.blit(startRecording, (465, 442))
+                    
+                
+                
 #-------------------------------------------------
     
 
